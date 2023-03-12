c19249822cacd7d2c80ac5e380bde70865d9e381
zdnMuh <2018044@scholar.itn.ac.id>
2023-03-12 13:35:50 +0700

    For beginner part 1

---
+++ DSBeginnerP1.py
@@ -0,0 +1,25 @@
+print("Hello World!")
+
+print("Hallo Dunia!")
+print("Riset Bahasa Python")
+
+# Statement
+print("Belajar Python menyenangkan")
+print("Halo Dunia")
+print("Hello World!")
+# Variables & Literals
+bilangan1 = 5
+bilangan2 = 10
+kalimat1 = "Belajar Bahasa Python"
+# Operators
+print(bilangan1 + bilangan2)
+
+bilangan1 = 20
+bilangan2 = 10
+print(bilangan1 - bilangan2)
+
+harga_asli = 20000
+potongan = 2000
+harga_setelah_potongan = harga_asli - potongan
+harga_final = harga_setelah_potongan*1.1
+print(harga_final)
