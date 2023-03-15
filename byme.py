f292bc505c3ed7e6d45b190700f8913002c0ae6e
zdnMuh <2018044@scholar.itn.ac.id>
2023-03-16 00:35:55 +0700

    Function for string

--- byme.py
+++ byme.py
@@ -1,6 +1,15 @@
-# remember for one case print should have one type of data, exm: str -> str, int -> int, float -> float
-inputNama = input("Masukkan nama anda: ")
-inputUmur = int(input("Masukkan umur anda: "))
+# # remember for one case print should have one type of data, exm: str -> str, int -> int, float -> float
+# inputNama = input("Masukkan nama anda: ")
+# inputUmur = int(input("Masukkan umur anda: "))
+# hasil = inputNama + " berumur " + str(inputUmur) + " tahun."
+# print(hasil)
 
-hasil = inputNama + " berumur " + str(inputUmur) + " tahun."
-print(hasil)
+# saldoAwal = int(input("Masukkan saldo awal: "))
+# deposit = int(input("Masukkan deposit: "))
+# saldoAkhir = saldoAwal + deposit
+# print("Saldo akhir: " + str(saldoAkhir))
+
+nama = 'muhammad zidan'
+print(nama.find('a'))
+print(len(nama))
+print(nama.upper())
