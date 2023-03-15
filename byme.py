f711471b7d35c2e440210e329d2398f86663510e
zdnMuh <2018044@scholar.itn.ac.id>
2023-03-15 20:15:23 +0700

    Input for python

---
+++ byme.py
@@ -0,0 +1,6 @@
+# remember for one case print should have one type of data, exm: str -> str, int -> int, float -> float
+inputNama = input("Masukkan nama anda: ")
+inputUmur = int(input("Masukkan umur anda: "))
+
+hasil = inputNama + " berumur " + str(inputUmur) + " tahun."
+print(hasil)
