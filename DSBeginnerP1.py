6b46e7106aa47a59f79241d942d9a4528981471a
zdnMuh <2018044@scholar.itn.ac.id>
2023-03-16 00:54:38 +0700

    Project calculator for beginner

--- DSBeginnerP1.py
+++ DSBeginnerP1.py
@@ -219,3 +219,23 @@
 # print(total_pengeluaran)
 # print(total_pemasukan)
 
+uang_jalan = 1500000
+jumlah_hari = 31
+list_plat_nomor = [8993, 2198, 2501, 2735, 3772, 4837, 9152]
+kendaraan_genap = 0
+kendaraan_ganjil = 0
+for plat_nomor in list_plat_nomor:
+    if plat_nomor%2 == 0:
+        kendaraan_genap+=1
+    else:
+        kendaraan_ganjil+=1
+
+i=1 #catatan untuk tanggal selalu dimulai dr 1
+total_pengeluaran=0
+while i<=jumlah_hari:
+    if i%2==0:
+        total_pengeluaran+=(kendaraan_genap*uang_jalan)
+    else:
+        total_pengeluaran+=(kendaraan_ganjil*uang_jalan)
+    i+=1
+print(total_pengeluaran)
