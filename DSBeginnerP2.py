5e2e601a942a81c0beba7cf8bb133545d8dadddb
zdnMuh <2018044@scholar.itn.ac.id>
2023-04-01 14:04:06 +0700

    Modify on tuple

--- DSBeginnerP2.py
+++ DSBeginnerP2.py
@@ -16,30 +16,78 @@
 # list_menu = list_makanan + list_minuman
 # print(list_menu)
 
-print('>>> fitur .append()')
-list_makanan = ['gado-gado', 'ayam goreng', 'rendang']
-list_makanan.append('ketoprak')
-print(list_makanan)
-print('>>> fitur .clear()')
-list_makanan = ['gado-gado', 'ayam goreng', 'rendang']
-list_makanan.clear()
-print(list_makanan)
-print('>>> fitur .copy()')
-list_makanan1 = ['gado-gado', 'ayam goreng', 'rendang']
-list_makanan2 = list_makanan1.copy()
-list_makanan3 = list_makanan1
-list_makanan2.append('opor')
-list_makanan3.append('ketoprak')
-print(list_makanan1)
-print(list_makanan2)
-print('>>> fitur .count()')
-list_score = ['budi', 'sud', 'budi', 'budi', 'budi',  'sud', 'sud']
-score_budi = list_score.count('budi')
-score_sud = list_score.count('sud')
-print(score_budi)
-print(score_sud)
-print('>>> fitur .extend()')
-list_menu = ['gado-gado', 'ayam goreng', 'rendang']
-list_minuman = ['es teh', 'es jeruk', 'es campur']
-list_menu.extend(list_minuman)
-print(list_menu)
+# print('>>> fitur .append()')
+# list_makanan = ['gado-gado', 'ayam goreng', 'rendang']
+# list_makanan.append('ketoprak')
+# print(list_makanan)
+# print('>>> fitur .clear()')
+# list_makanan = ['gado-gado', 'ayam goreng', 'rendang']
+# list_makanan.clear()
+# print(list_makanan)
+# print('>>> fitur .copy()')
+# list_makanan1 = ['gado-gado', 'ayam goreng', 'rendang']
+# list_makanan2 = list_makanan1.copy()
+# list_makanan3 = list_makanan1
+# list_makanan2.append('opor')
+# list_makanan3.append('ketoprak')
+# print(list_makanan1)
+# print(list_makanan2)
+# print('>>> fitur .count()')
+# list_score = ['budi', 'sud', 'budi', 'budi', 'budi',  'sud', 'sud']
+# score_budi = list_score.count('budi')
+# score_sud = list_score.count('sud')
+# print(score_budi)
+# print(score_sud)
+# print('>>> fitur .extend()')
+# list_menu = ['gado-gado', 'ayam goreng', 'rendang']
+# list_minuman = ['es teh', 'es jeruk', 'es campur']
+# list_menu.extend(list_minuman)
+# print(list_menu)
+
+# # Fitur .index()
+# print(">>> Fitur .index()")
+# list_score = ['Budi','Sud','Budi','Budi','Budi','Sud','Sud']
+# score_pertama_sud = list_score.index('Sud') + 1
+# print(score_pertama_sud)
+# # Fitur .insert()
+# print(">>> Fitur .insert()")
+# list_score = ['Budi','Sud','Budi','Budi','Sud']
+# list_score.insert(3,'sud')
+# print(list_score)
+# # Fitur .pop()
+# print(">>> Fitur .pop()")
+# list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+# list_menu.pop(1)
+# print(list_menu)
+# # Fitur .remove()
+# print(">>> Fitur .remove()")
+# list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+# list_menu.remove('rendang')
+# print(list_menu)
+# # Fitur .reverse()
+# print(">>> Fitur .reverse()")
+# list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+# list_menu.reverse()
+# print(list_menu)
+# # Fitur .sort()
+# print(">>> Fitur .sort()")
+# list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+# list_menu.sort()
+# print(list_menu)
+# list_menu.sort(reverse=True)
+# print(list_menu)
+
+# # Fitur .count()
+# print(">>> Fitur .count()")
+# tuple_score = ('Budi', 'Sud', 'Budi', 'Budi', 'Budi', 'Sud', 'Sud')
+# score_budi = tuple_score.count('Budi')
+# score_sud = tuple_score.count('Sud')
+# print(score_budi) # akan menampilkan output 4
+# print(score_sud) # akan menampilkan output 3
+# # Fitur .index()
+# print(">>> Fitur .index()")
+# tuple_score = ('Budi','Sud','Budi','Budi','Budi','Sud','Sud')
+# score_pertama_sud = tuple_score.index('Sud') + 1
+# print(score_pertama_sud) # akan menampilkan output 2
+
+print(saya)
