27b3c3f77502e1f4ec90b64c97ceab3f0f57fa60
zdnMuh <2018044@scholar.itn.ac.id>
2023-04-08 13:54:19 +0700

    Little change

--- DSBeginnerP2.py
+++ DSBeginnerP2.py
@@ -90,4 +90,87 @@
 # score_pertama_sud = tuple_score.index('Sud') + 1
 # print(score_pertama_sud) # akan menampilkan output 2
 
-print(saya)
+# # Fitur .add()
+# print(">>> Fitur .add()")
+# set_buah = {'Jeruk','Apel','Anggur'}
+# set_buah.add('Melon')
+# print(set_buah)
+# # Fitur .clear()
+# print(">>> Fitur .clear()")
+# set_buah = {'Jeruk','Apel','Anggur'}
+# set_buah.clear()
+# print(set_buah)
+# # Fitur .copy()
+# print(">>> Fitur .copy()")
+# set_buah1 = {'Jeruk','Apel','Anggur'}
+# set_buah2 = set_buah1
+# set_buah3 = set_buah1.copy()
+# set_buah2.add('Melon')
+# set_buah3.add('Kiwi')
+# print(set_buah1)
+# print(set_buah2)
+# # Fitur .update()
+# print(">>> Fitur .update()")
+# parcel1 = {'Anggur','Apel','Jeruk'}
+# parcel2 = {'Apel','Kiwi','Melon'}
+# parcel1.update(parcel2)
+# print(parcel1)
+# # Fitur .pop()
+# print(">>> Fitur .pop()")
+# parcel = {'Anggur','Apel','Jeruk'}
+# buah = parcel.pop()
+# print(buah)
+# print(parcel)
+# # Fitur .remove()
+# print(">>> Fitur .remove()")
+# parcel = {'Anggur','Apel','Jeruk'}
+# parcel.remove('Apel')
+# print(parcel)
+
+# # Fitur .union()
+# print(">>> Fitur .union()")
+# parcel1 = {'Anggur','Apel','Jeruk'}
+# parcel2 = {'Apel','Kiwi','Melon'}
+# parcel3 = parcel1.union(parcel2)
+# print(parcel1)
+# print(parcel3)
+# # Fitur .isdisjoint()
+# print(">>> Fitur .isdisjoint()")
+# parcel1 = {'Anggur','Apel','Jeruk'}
+# parcel2 = {'Kiwi','Melon','Pisang'}
+# parcel3 = {'Apel','Srikaya','Semangka'}
+# parcel1_parcel2_disjoint = parcel1.isdisjoint(parcel2)
+# print(parcel1_parcel2_disjoint)
+# parcel1_parcel3_disjoint = parcel1.isdisjoint(parcel3)
+# print(parcel1_parcel3_disjoint)
+# # Fitur .issubset()
+# print(">>> Fitur .issubset()")
+# parcel_A = {'Anggur', 'Apel'}
+# parcel_B = {'Durian','Semangka','Apel'}
+# parcel_C = {'Anggur', 'Kiwi', 'Apel', 'Jeruk', 'Melon'}
+# parcel_A_dalam_C = parcel_A.issubset(parcel_C)
+# parcel_B_dalam_C = parcel_B.issubset(parcel_C)
+# print(parcel_A_dalam_C)
+# print(parcel_B_dalam_C)
+# # Fitur .issuperset()
+# print(">>> Fitur .issuperset()")
+# parcel_C_mengandung_A = parcel_C.issuperset(parcel_A)
+# parcel_C_mengandung_B = parcel_C.issuperset(parcel_B)
+# print(parcel_C_mengandung_A)
+# print(parcel_C_mengandung_B)
+# # Fitur .intersection()
+# print(">>> Fitur .intersection()")
+# parcel_A = {'Anggur', 'Kiwi', 'Apel', 'Jeruk', 'Melon'}
+# parcel_B = {'Apel', 'Jeruk', 'Semangka', 'Durian', 'Tomat'}
+# parcel_C = parcel_A.intersection(parcel_B)
+# print(parcel_C)
+# # Fitur .difference()
+# print(">>> Fitur .difference()")
+# parcel_C = parcel_A.difference(parcel_B)
+# print(parcel_C)
+# # Fitur .symmetric_difference()
+# print(">>> Fitur .symmetric_difference()")
+# parcel_A = {'Anggur', 'Apel', 'Jeruk', 'Melon'}
+# parcel_B = {'Apel','Jeruk','Semangka','Leci'}
+# parcel_C = parcel_A.symmetric_difference(parcel_B)
+# print(parcel_C)
