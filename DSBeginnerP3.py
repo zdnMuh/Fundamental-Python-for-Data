6a1436b38df30ddcd2131e7c9ccd39c9bda233d4
zdnMuh <2018044@scholar.itn.ac.id>
2023-04-08 13:55:08 +0700

    OOP python

---
+++ DSBeginnerP3.py
@@ -0,0 +1,9 @@
+class Karyawan:
+    nama_perusahaan = 'ABC'
+
+aksara = Karyawan()
+senja = Karyawan()
+print(aksara.nama_perusahaan)
+aksara.nama_perusahaan = 'DEF'
+print(aksara.nama_perusahaan)
+print(senja.nama_perusahaan)
